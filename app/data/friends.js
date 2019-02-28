983448d1594992b40d1e4970a9124f935a1d9ce5
Ashinator <Ahart425@gmail.com>
2019-02-28 16:15:39 -0700

    pseudo code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var surveyArray = [
     {
     name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg",
     scores:[
         5,
         1,
@@ -13,7 +13,67 @@ var surveyArray = [
         5,
         4,
         1
-    ]}
+    ]},
+    {
+      name:"Sherry",
+      photo:"https://beartrust.org/wp-content/uploads/2016/08/Bear-Den-photo.jpg",
+      scores:[
+          2,
+          3,
+          4,
+          1,
+          5,
+          3,
+          2,
+          3,
+          5,
+          4
+      ]},
+      {
+        name:"Jonesy",
+        photo:"https://i.ytimg.com/vi/1R-QFQGWYuc/maxresdefault.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            2,
+            3,
+            3,
+            2,
+            5,
+            4,
+            2
+        ]},
+        {
+          name:"Parker",
+          photo:"https://mountainjournal.org/content/articles/ic_1509058466_780x_false.jpg",
+          scores:[
+              4,
+              1,
+              2,
+              3,
+              3,
+              2,
+              5,
+              2,
+              1,
+              4
+          ]},
+          {
+            name:"Twila",
+            photo:"https://www.telegraph.co.uk/content/dam/news/2016/08/23/106598324PandawaveNEWS-xlarge_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg",
+            scores:[
+                5,
+                1,
+                3,
+                5,
+                2,
+                3,
+                2,
+                5,
+                4,
+                3
+            ]}
     ];
   
   module.exports = surveyArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,40 @@
 var surveyData = require("../data/friends.js");
 
 
-module.exports = function(app) {
- 
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+
+  app.get("/api/friends", function (req, res) {
     res.json(surveyData);
   });
 
-app.post("/api/friends", function(req, res) {
-    surveyData.push(req.body);
-    res.json(true);
-})
+  app.post("/api/friends", function (req, res) {
+    // surveyData.push(req.body);
 
-  app.post("/api/clear", function(req, res) {
+    // let bestMatch = {name: photo: }
+
+    // Create a variable to hold user scores (ie: Ahmed's array of scores)
+
+    // Variable to hold the totalDifference
+
+    // Set up for Loop to go through SurveyData (Friend's list)
+    // Set up another for Loop inside ^ this loop (j loop) to go through score's array - Compare the user's answers to the potential friends answers (Ahmed, Carol, Sherry)
+
+    // totalDifference - Math.abs (returns absolute value) - pass in potential friend score vs user score
+    // 1st time around, compare to first friend - if/else - 
+
+
+
+    res.json(surveyData[0]);
+  })
+
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
-    
+
     surveyData.length = [];
 
-    res.json({ ok: true });
+    res.json({
+      ok: true
+    });
   });
 };
